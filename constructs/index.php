1846def7713a99f4c133229fa1b79678807d2de5
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-08-02 11:19:21 +0100

    Classes

---
+++ constructs/index.php
@@ -0,0 +1,23 @@
+<?php
+// classes are created by following the nouns in the problem statement
+class Invoice 
+{
+
+}
+
+class InvoiceItem
+{
+
+}
+
+class AchievementBadge
+{
+    public $title;
+    public $description;
+    public $points;
+
+    public function awardTo($user) 
+    {
+
+    }
+}
