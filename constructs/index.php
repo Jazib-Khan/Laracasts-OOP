bb47d8a0361241b33dbada06ffde03b7ce05dcfb
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-08-02 14:31:57 +0100

    Abstract classes

--- constructs/index.php
+++ constructs/index.php
@@ -1,49 +1,40 @@
 <?php
+// abstract key words removes the ability to instantiate the class directly, it's a template of sorts.
+// we can have abstract classes and methods. Abstract methods are a requirements so if the class is extended it must be included.
 
-class Collection 
+abstract class AchievementType
 {
-    protected array $items;
-
-    public function __construct(array $items) 
+    public function name()
     {
-        $this->items = $items;
+        $class = (new ReflectionClass($this))->getShortName();
+
+        return trim(preg_replace('/[A-Z]/', ' $0', $class));
     }
 
-    public function sum($key) 
+    public function icon()
     {
-        return array_sum(array_column($this->items, $key));
+        return strtolower(str_replace(' ', '-', $this->name())). '.png';
+    }
+
+    abstract public function qualifier($user);
+}
+
+class FirstThousandPoints extends AchievementType
+{
+    public function qualifier($user)
+    {
+
+    }
+} 
+
+class ReachTop50 extends AchievementType
+{
+    public function qualifier($user)
+    {
+
     }
 }
 
-class VideosCollection extends Collection 
-{
-    public function length() 
-    {
-        return $this->sum('length');
-    }
-
-    public function titles() 
-    {
-        return $this->sum('title');
-    }
-}
-
-class Video 
-{
-    public $title;
-    public $length;
-
-    public function __construct($title, $length)
-    {
-        $this->title = $title;
-        $this->length = $length;
-    }
-}
-
-$videos = new VideosCollection ([
-    new Video('Some Video', 100),
-    new Video('Some Video 2', 200),
-    new Video('Some Video 3', 300),
-]);
-
-echo $videos->length();
+$achievement = new FirstThousandPoints();
+echo $achievement->name(); 
+echo $achievement->icon(); 
